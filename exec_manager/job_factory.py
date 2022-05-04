a19747cdfe271a91c8ef8cd91867fe6c6910752a
e-buerger <e.buerger@dkfz-heidelberg.de>
2022-05-04 11:23:25 +0200

    adapt code for passing precommit

--- exec_manager/job_factory.py
+++ exec_manager/job_factory.py
@@ -1,8 +1,7 @@
 """class for job factory"""
 
-import job
-
 from exec_manager.exec_profile import ExecProfile
+from exec_manager.job import Job
 
 
 class JobFactory:
@@ -16,12 +15,12 @@ class JobFactory:
 
     Methods
     -------
-    create_job(inputs: dict, workflow, exec_profile: ExecProfile) -> job:
+    create_job(inputs: dict, workflow, exec_profile: ExecProfile) -> Job:
         creates a new job
     """
 
     def __init__(self) -> None:
         """this is the constructor"""
 
-    def create_job(self, inputs: dict, workflow, exec_profile: ExecProfile) -> job:
+    def create_job(self, inputs: dict, workflow, exec_profile: ExecProfile) -> Job:
         """this method creates a new job"""
